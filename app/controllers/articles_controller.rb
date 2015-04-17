bb5a8e348babfc9d2057ba45c5e13543081b804c
colleowino <colleowino@gmail.com>
2015-04-17 13:06:40 +0300

    5.8 works

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,12 @@ class ArticlesController < ApplicationController
 	def new 
 	end
 
+	def show
+	   @article = Article.find(params[:id])
+	 end
+	 
 	def create
-		@article = Article.new(params[:article])
+		@article = Article.new(params.require(:article).permit(:title, :text))
 		 
 		  @article.save
 		  redirect_to @article
@@ -13,5 +17,5 @@ class ArticlesController < ApplicationController
 	  def article_params
 	    params.require(:article).permit(:title, :text)
 	  end
-	  
+
 end
