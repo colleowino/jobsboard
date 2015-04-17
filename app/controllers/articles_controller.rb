ba2c79f5c78e460f11e9630a305b0a3f7b04d429
colleowino <colleowino@gmail.com>
2015-04-17 13:54:09 +0300

    5.10 working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,26 @@
 class ArticlesController < ApplicationController
 	def new 
+		@article = Article.new
 	end
 
 	def show
 	   @article = Article.find(params[:id])
 	 end
-	 
-	def create
-		@article = Article.new(params.require(:article).permit(:title, :text))
-		 
-		  @article.save
-		  redirect_to @article
-	end
+
+def create
+  @article = Article.new(article_params)
+ 
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
+end
 
 	private
 	  def article_params
 	    params.require(:article).permit(:title, :text)
 	  end
+
 
 end
