1017eb0a0170ae43a2f8378c3476ea260ac91e1a
colleowino <colleowino@gmail.com>
2017-02-06 17:52:09 +0300

    enabled save function jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,4 +5,14 @@ class JobsController < ApplicationController
   def new
   	@job = Job.new
   end
+
+  def create
+  	@job = Job.new
+	  if @job.save
+	    redirect_to root_path
+	  else
+	    render "new"
+	  end
+  end
+
 end
